0a888137f6d387c61eea0d8838db95d197697af9
Bryan VIDAL <b.vidal@lonsdale.fr>
2021-03-06 00:04:02 +0100

    set starterkit

--- wp-content/themes/laforce/front-page.php
+++ wp-content/themes/laforce/front-page.php
@@ -5,13 +5,9 @@ Template Name: Homepage
 
 get_header();
 
-$thumbsInsta = get_instagram_thumbs();
 ?>
 
 
-<?php foreach ($thumbsInsta as $thumbsInstaItem): ?>
-    <img src="<?= $thumbsInstaItem->media_url; ?>" width="50%" alt="">
-<?php endforeach; ?>
 
 
 <?php
